ea205171ad84e8818c28d3128bb790b71c577350
623Mark <bu_chanon@kkumail.com>
2022-06-03 13:01:44 +0700

    5# fix build function compound error

--- Installer/debian/duplicati-launcher.sh
+++ Installer/debian/duplicati-launcher.sh
@@ -3,9 +3,9 @@ INSTALLDIR=/usr/lib/duplicati
 export LD_LIBRARY_PATH="${INSTALLDIR}${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
 export MONO_PATH=$MONO_PATH:${INSTALLDIR}
 
-# EXE_FILE=${INSTALLDIR}/Duplicati.GUI.TrayIcon.exe
-# APP_NAME=Duplicati
-EXE_FILE=${INSTALLDIR}/ArakDataProtection.TrayIcon.exe
-APP_NAME=Arak
+EXE_FILE=${INSTALLDIR}/Duplicati.GUI.TrayIcon.exe
+APP_NAME=Duplicati
+# EXE_FILE=${INSTALLDIR}/ArakDataProtection.TrayIcon.exe
+# APP_NAME=Arak
 
 exec -a "$APP_NAME" mono "$EXE_FILE" "$@"
